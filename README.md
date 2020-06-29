e2c278850a068272f01f38032ba47575f6c1fb19
Yesu Carter <ycarter@colgate.edu>
2020-06-29 15:36:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Personal Portfolio
+
+Image Credits
+- CFA Logo https://logosandtypes.com/alphabet/letter-c/chick-fil-a/
+- CUFD
+<span>Photo by <a href="https://unsplash.com/@nate_dumlao?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Nathan Dumlao</a> on <a href="/s/photos/building-blocks?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+const express = require('express');
+var liveServer = require('live-server');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+  response.sendFile('./index.html');
+});
+
+liveServer.start({
+  port: 3001,
+  host: "127.0.0.1",
+  root: "public"
+  });
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
